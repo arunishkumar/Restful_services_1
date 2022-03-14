4b4d8735b11815b7f6dc0543b94244585c5e0995
arunishkumar <arunish19@gmail.com>
2022-03-15 01:16:50 +0530

    File committed

---
+++ src/main/java/com/arunish/project/exercise_application/WelcomeMessage.java
@@ -0,0 +1,14 @@
+/*package com.arunish.project.exercise_application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeMessage {
+     
+	@GetMapping("/PrintMessage")
+	public String printMessage()
+	{
+		return "Welcome to Spring Boot";
+	}
+} */
